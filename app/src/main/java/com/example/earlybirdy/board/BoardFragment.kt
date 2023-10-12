926ef9bc8dfaf3d0f0035af823060efc1ef461cc
LeeChoongHwan <choo901@naver.com>
2023-10-12 19:26:51 +0900

    feature : Fragment Binding 추가

--- app/src/main/java/com/example/earlybirdy/board/BoardFragment.kt
+++ app/src/main/java/com/example/earlybirdy/board/BoardFragment.kt
@@ -6,13 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.earlybirdy.R
+import com.example.earlybirdy.databinding.FragmentBoardBinding
 
 class BoardFragment : Fragment() {
+
+    private var _binding: FragmentBoardBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_board, container, false)
+       _binding = FragmentBoardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
--- app/src/main/java/com/example/earlybirdy/home/HomeFragment.kt
+++ app/src/main/java/com/example/earlybirdy/home/HomeFragment.kt
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.earlybirdy.R
+import com.example.earlybirdy.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 
 }
