7cbb955c986120198cfc2e30f75a7b70380ea078
LDH <edh7054@gmail.com>
2023-10-13 15:03:57 +0900

    feature/ui: 로그인 회원가입 페이지 ui 수정

--- app/src/main/java/com/example/earlybirdy/MainActivity.kt
+++ app/src/main/java/com/example/earlybirdy/MainActivity.kt
@@ -2,10 +2,58 @@ package com.example.earlybirdy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import com.example.earlybirdy.util.navigateToSigninActivity
+import com.example.earlybirdy.util.showToast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+    //val db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+        database = Firebase.database.reference
+
+        val btnSignout: Button = findViewById<Button>(R.id.btn_signout_test)
+        btnSignout.setOnClickListener {
+            logOut()
+        }
+    }
+
+    private fun login() {
+        val tvTest = findViewById<TextView>(R.id.tv_test)
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            showToast(this, "로그인 중 = ${currentUser.email}")
+            tvTest.setText("${currentUser.uid}")
+        } else {
+            navigateToSigninActivity(this)
+        }
+    }
+
+    private fun logOut() {
+        val tvTest = findViewById<TextView>(R.id.tv_test)
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            auth.signOut()
+            showToast(this, "로그아웃 성공!")
+            navigateToSigninActivity(this)
+        } else {
+            showToast(this, "로그인을 먼저 해주세요")
+            tvTest.setText("${currentUser?.uid}")
+            navigateToSigninActivity(this)
+        }
     }
 }
+
--- app/src/main/java/com/example/earlybirdy/signin/SigninActivity.kt
+++ app/src/main/java/com/example/earlybirdy/signin/SigninActivity.kt
@@ -2,11 +2,67 @@ package com.example.earlybirdy.signin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.earlybirdy.R
+import com.example.earlybirdy.databinding.ActivitySigninBinding
+import com.example.earlybirdy.util.navigateToMainActivity
+import com.example.earlybirdy.util.navigateToSignupActivity
+import com.example.earlybirdy.util.showToast
+import com.google.firebase.auth.FirebaseAuth
 
 class SigninActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivitySigninBinding.inflate(layoutInflater) }
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        //로그인
+        binding.btnSignin.setOnClickListener {
+            signIn()
+        }
+
+        // 나가기 = 앱 종료
+        binding.tvFinish.setOnClickListener {
+            //val intent = Intent(Intent.ACTION_VIEW, Uri.parse("${}"))
+            moveTaskToBack(true)
+            finish()
+            android.os.Process.killProcess(android.os.Process.myPid())
+        }
+
+        // 회원가입 페이지로 이동
+        binding.tvBtnSignup.setOnClickListener {
+            //val intent = Intent(Intent.ACTION_VIEW, Uri.parse("${}"))
+            navigateToSignupActivity(this)
+        }
+    }
+
+    //로그인 함수
+    private fun signIn() {
+        val email = binding.titEmail.text.toString()
+        val password = binding.titPassword.text.toString()
+
+        if (email.isNotBlank() && password.isNotBlank()) {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        showToast(this, "로그인 성공")
+                        navigateToMainActivity(this)
+                        finish()
+                    } else {
+                        showToast(this, "로그인 실패!")
+                    }
+                }
+        } else {
+            showToast(this, "이메일 및 비밀번호를 입력해주세요")
+        }
     }
 }
+
+//finish() : 이 코드가 속해있는 액티비티를 종료 onDestroy()를 호출
+//system.exit(0) : 현재 액티비티를 종료
+//android.os.Process.killProcess(android.os.Process.myPid()) : 현재의 프로세스 및 서비스를 종료
+//moveTaskToBack(boolean): 현재 어플을 백그라운드로 넘김 /  현재 실행되고있는 어플이 하나라면 홈화면으로 바귀지만 종료는 아님
