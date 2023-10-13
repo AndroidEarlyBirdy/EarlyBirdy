4000225c38c93b59f19a5267bd2e0e7bd390c278
Odin5din <133902344+Odin5din@users.noreply.github.com>
2023-10-13 17:53:51 +0900

    ui : Edit_Profile 페이지 layout 및 디자인 작업

--- app/src/main/java/com/example/earlybirdy/edit_profile/EditProfileActivity.kt
+++ app/src/main/java/com/example/earlybirdy/edit_profile/EditProfileActivity.kt
@@ -2,11 +2,29 @@ package com.example.earlybirdy.edit_profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.example.earlybirdy.R
+import com.example.earlybirdy.databinding.ActivityEditProfileBinding
 
 class EditProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_profile)
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvEditSave.setOnClickListener()
+
+        binding.etProfilePassword.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                if (binding.etProfilePassword.getText().toString().equals(binding.etProfilePasswordCheck.getText().toString())){
+
+                } else {
+
+                }
+
+            }
+        })
     }
 }
