cba1401d7328f384be7b8815e706b8cab548b272
minji05 <smj95133@likelion.org>
2023-10-17 15:43:34 +0900

    feature : 버튼 intent 처리(createplan 페이지로)

--- app/src/main/java/com/example/earlybirdy/home/HomeFragment.kt
+++ app/src/main/java/com/example/earlybirdy/home/HomeFragment.kt
@@ -3,14 +3,18 @@ package com.example.earlybirdy.home
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.earlybirdy.create_plan.CreatePlanActivity
 import com.example.earlybirdy.databinding.FragmentHomeBinding
 import com.example.earlybirdy.util.navigateToAlarmActivity
+import com.example.earlybirdy.main.MainActivity
 
+@Suppress("UNREACHABLE_CODE")
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
@@ -27,6 +31,12 @@ class HomeFragment : Fragment() {
         binding.ivGoAlarm.setOnClickListener {
             navigateToAlarmActivity(this.requireActivity())
         }
+
+        binding.btnCreate.setOnClickListener {
+            val intent = Intent(requireContext(), CreatePlanActivity::class.java)
+            startActivity(intent)
+        }
+
         return binding.root
     }
 
@@ -37,6 +47,7 @@ class HomeFragment : Fragment() {
         binding.tvAlarm.text = "${pref.getInt("hour", 0).toString()} : ${pref.getInt("min", 0).toString()} AM"
     }
 
+
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
@@ -45,4 +56,5 @@ class HomeFragment : Fragment() {
     companion object {
         fun newInstance() = HomeFragment()
     }
+
 }
