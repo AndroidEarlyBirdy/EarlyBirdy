e264a10ee47229f2c22ab40fb392232d9b714ab3
LDH <edh7054@gmail.com>
2023-10-17 16:08:16 +0900

    PR 전 임시 커밋

--- app/src/main/java/com/example/earlybirdy/alarm/AlarmActivity.kt
+++ app/src/main/java/com/example/earlybirdy/alarm/AlarmActivity.kt
@@ -1,13 +1,25 @@
 package com.example.earlybirdy.alarm
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.TimePickerDialog
 import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.example.earlybirdy.R
 import com.example.earlybirdy.databinding.ActivityAlarmBinding
 import com.example.earlybirdy.databinding.ActivitySigninBinding
+import com.example.earlybirdy.main.MainActivity
 import com.example.earlybirdy.util.navigateToMainActivity
 import java.text.SimpleDateFormat
 import java.util.Calendar
@@ -15,11 +27,19 @@ import java.util.Calendar
 class AlarmActivity : AppCompatActivity() {
     private val binding by lazy { ActivityAlarmBinding.inflate(layoutInflater) }
 
+    private var handler: Handler = Handler() // Notification delay적용을 위한 Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
 //        setTimeChangedListener()
+
+//        if (binding.switchAlarm.isChecked){
+//            handler.postDelayed({
+//                setNotification()
+//            }, binding.tpSetTime.minute.toLong())
+//        }
 
         //오전 오후 설정
         binding.tpSetTime.setIs24HourView(true)
@@ -32,9 +52,9 @@ class AlarmActivity : AppCompatActivity() {
         // 저장버튼 누르면 상태 저장
         binding.tvSave.setOnClickListener {
             saveTime()
-//            setNotification()
-            navigateToMainActivity(this)
-            finish()
+            setNotification()
+//            navigateToMainActivity(this)
+//            finish()
         }
     }
 //    private fun setTimeChangedListener() {
@@ -57,6 +77,48 @@ class AlarmActivity : AppCompatActivity() {
     }
 
     private fun setNotification() {
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val builder: NotificationCompat.Builder
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            //26버전 이상
+            val channelId="channel"
+            val channelName="Number Channel"
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply{
+                description = "My Channel One Description"
+                setShowBadge(true)
+                val uri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                val audioAttributes = AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .build()
+                setSound(uri, audioAttributes)
+                enableVibration(true)
+            }
+            manager.createNotificationChannel(channel)
+            builder = NotificationCompat.Builder(this, channelId)
+
+        }else{
+            builder = NotificationCompat.Builder(this)
+        }
+
+//        val bitmap = BitmapFactory.decodeResource(resources,R.drawable.contactdetailfragment_phonecall)
+        val intent = Intent(this,MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+        builder.run{
+            setSmallIcon(R.mipmap.ic_launcher)
+            setWhen(System.currentTimeMillis())
+            setContentTitle("EarlyBirdy")
+            setContentText("일어나세요 용사여!")
+//            setLargeIcon(bitmap)
+            addAction(R.mipmap.ic_launcher,"Action",pendingIntent)
+        }
+        manager.notify(1,builder.build())
 
     }
 }
