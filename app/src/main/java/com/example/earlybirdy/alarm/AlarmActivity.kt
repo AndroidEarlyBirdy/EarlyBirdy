2391abb5b9ebedf2418798d3e72a2391be9ff021
LDH <edh7054@gmail.com>
2023-10-17 11:54:55 +0900

    PR 전 임시 커밋

--- app/src/main/java/com/example/earlybirdy/alarm/AlarmActivity.kt
+++ app/src/main/java/com/example/earlybirdy/alarm/AlarmActivity.kt
@@ -1,11 +1,15 @@
 package com.example.earlybirdy.alarm
 
+import android.app.TimePickerDialog
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.earlybirdy.R
 import com.example.earlybirdy.databinding.ActivityAlarmBinding
 import com.example.earlybirdy.databinding.ActivitySigninBinding
 import com.example.earlybirdy.util.navigateToMainActivity
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 class AlarmActivity : AppCompatActivity() {
     private val binding by lazy { ActivityAlarmBinding.inflate(layoutInflater) }
@@ -14,6 +18,7 @@ class AlarmActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+
         // 취소 버튼 누르면 액티비티 종료
         binding.tvCancel.setOnClickListener {
             finish()
@@ -21,8 +26,21 @@ class AlarmActivity : AppCompatActivity() {
 
         // 저장버튼 누르면 상태 저장
         binding.tvSave.setOnClickListener {
+
             navigateToMainActivity(this)
             finish()
         }
     }
+
+    private fun setTime() {
+        binding.tpSetTime.setOnTimeChangedListener { view, hourOfDay, minute ->
+
+        }
+    }
+
+    private fun saveTime(){
+//        val pref = getPreferences("alarm time", 0)
+
+    }
+
 }
