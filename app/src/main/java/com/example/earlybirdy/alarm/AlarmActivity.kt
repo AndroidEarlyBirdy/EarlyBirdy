59df32899d28ba44f09209ded4c2ad1f8f3fcf48
LDH <edh7054@gmail.com>
2023-10-17 21:00:54 +0900

    PR 전 임시 커밋

--- app/src/main/java/com/example/earlybirdy/alarm/AlarmActivity.kt
+++ app/src/main/java/com/example/earlybirdy/alarm/AlarmActivity.kt
@@ -59,8 +59,10 @@ class AlarmActivity : AppCompatActivity() {
 
         // 저장버튼 누르면 상태 저장
         binding.tvSave.setOnClickListener {
+            val pref = getSharedPreferences("alarmTime",0)
             saveTime()
-            createNotificationChannel()
+            //if ()
+            sendNotification()
 //            navigateToMainActivity(this)
 //            finish()
         }
@@ -84,87 +86,87 @@ class AlarmActivity : AppCompatActivity() {
         edit.apply()
     }
 
-    private fun createNotificationChannel() {
+//    private fun createNotificationChannel() {
+//        val pref = getSharedPreferences("alarmTime",0)
+//        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+//        val builder: NotificationCompat.Builder
+//
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+//            //26버전 이상
+//            val channelId="channel"
+//            val channelName="Number Channel"
+//            val channel = NotificationChannel(
+//                channelId,
+//                channelName,
+//                NotificationManager.IMPORTANCE_DEFAULT
+//            ).apply{
+//                description = "My Channel One Description"
+//                setShowBadge(true)
+//                val uri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+//                val audioAttributes = AudioAttributes.Builder()
+//                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+//                    .setUsage(AudioAttributes.USAGE_ALARM)
+//                    .build()
+//                setSound(uri, audioAttributes)
+//                enableVibration(true)
+//            }
+//            manager.createNotificationChannel(channel)
+//            builder = NotificationCompat.Builder(this, channelId)
+//
+//        }else{
+//            builder = NotificationCompat.Builder(this)
+//        }
+//
+//        val bitmap = BitmapFactory.decodeResource(resources,R.drawable.ic_logo)
+//        val intent = Intent(this,MainActivity::class.java)
+//        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//        val pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+//        builder.run{
+//            setSmallIcon(R.mipmap.ic_launcher) // 작은 아이콘
+//            setWhen(System.currentTimeMillis())
+//            setContentTitle("${pref.getInt("hour", 0).toString()} : ${pref.getInt("min", 0).toString()}") // 제목
+//            setContentText("일어나세요 용사여!") // 본문
+////            setLargeIcon(bitmap)
+//            addAction(R.mipmap.ic_launcher,"Action",pendingIntent)
+//        }
+//        manager.notify(1,builder.build())
+//
+//    }
+
+    private fun sendNotification() {
         val pref = getSharedPreferences("alarmTime",0)
-        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        val builder: NotificationCompat.Builder
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            //26버전 이상
-            val channelId="channel"
-            val channelName="Number Channel"
-            val channel = NotificationChannel(
-                channelId,
-                channelName,
-                NotificationManager.IMPORTANCE_DEFAULT
-            ).apply{
-                description = "My Channel One Description"
-                setShowBadge(true)
-                val uri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-                val audioAttributes = AudioAttributes.Builder()
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                    .setUsage(AudioAttributes.USAGE_ALARM)
-                    .build()
-                setSound(uri, audioAttributes)
-                enableVibration(true)
-            }
-            manager.createNotificationChannel(channel)
-            builder = NotificationCompat.Builder(this, channelId)
+        val title = "${pref.getInt("hour", 0).toString()} : ${pref.getInt("min", 0).toString()} AM"
+        val message = "일어나세요 용사여"
 
-        }else{
-            builder = NotificationCompat.Builder(this)
+        val intent = Intent(this, MainActivity::class.java)
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE)
+                as NotificationManager
+        val notificationID = 1
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
         }
 
-        val bitmap = BitmapFactory.decodeResource(resources,R.drawable.ic_logo)
-        val intent = Intent(this,MainActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        val pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
-        builder.run{
-            setSmallIcon(R.mipmap.ic_launcher) // 작은 아이콘
-            setWhen(System.currentTimeMillis())
-            setContentTitle("${pref.getInt("hour", 0).toString()} : ${pref.getInt("min", 0).toString()}") // 제목
-            setContentText("일어나세요 용사여!") // 본문
-//            setLargeIcon(bitmap)
-            addAction(R.mipmap.ic_launcher,"Action",pendingIntent)
-        }
-        manager.notify(1,builder.build())
+        val pendingIntent = getActivity(this, 0, intent, FLAG_ONE_SHOT or PendingIntent.FLAG_MUTABLE)
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setSmallIcon(R.drawable.ic_logo)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .build()
 
+        notificationManager.notify(notificationID, notification)
     }
 
-//    private fun sendNotification() {
-//        val pref = getSharedPreferences("alarmTime",0)
-//
-//        val title = "${pref.getInt("hour", 0).toString()} : ${pref.getInt("min", 0).toString()} AM"
-//        val message = "일어나세요 용사여"
-//
-//        val intent = Intent(this, MainActivity::class.java)
-//        val notificationManager = getSystemService(NOTIFICATION_SERVICE)
-//                as NotificationManager
-//        val notificationID = 1
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            createNotificationChannel(notificationManager)
-//        }
-//
-//        val pendingIntent = getActivity(this, 0, intent, FLAG_ONE_SHOT or PendingIntent.FLAG_MUTABLE)
-//        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-//            .setContentTitle(title)
-//            .setContentText(message)
-//            .setSmallIcon(R.drawable.ic_logo)
-//            .setAutoCancel(true)
-//            .setContentIntent(pendingIntent)
-//            .build()
-//
-//        notificationManager.notify(notificationID, notification)
-//    }
-//
-//    @RequiresApi(Build.VERSION_CODES.O)
-//    private fun createNotificationChannel(notificationManager: NotificationManager) {
-//        val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, IMPORTANCE_HIGH).apply {
-//            description = "Channel Description"
-//            enableLights(true)
-//            lightColor = Color.GREEN
-//        }
-//        notificationManager.createNotificationChannel(channel)
-//    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, IMPORTANCE_HIGH).apply {
+            description = "Channel Description"
+            enableLights(true)
+            lightColor = Color.GREEN
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
 }
