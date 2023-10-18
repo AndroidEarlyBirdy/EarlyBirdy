955cc58aa35bba1775846bcf24d9efa658aa039e
minji05 <smj95133@likelion.org>
2023-10-18 20:47:00 +0900

    feature :   목표 리스트 불러오기

--- app/src/main/java/com/example/earlybirdy/home/HomeFragment.kt
+++ app/src/main/java/com/example/earlybirdy/home/HomeFragment.kt
@@ -2,32 +2,41 @@ package com.example.earlybirdy.home
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
 import android.content.SharedPreferences
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.earlybirdy.create_plan.CreatePlanActivity
+import com.example.earlybirdy.data.MyGoal
 import com.example.earlybirdy.databinding.FragmentHomeBinding
+import com.example.earlybirdy.databinding.ItemTodoMainBinding
 import com.example.earlybirdy.util.navigateToAlarmActivity
+import com.example.earlybirdy.main.MainActivity
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
 import java.util.UUID
 
+
 class HomeFragment : Fragment() {
 
-    var auth: FirebaseAuth? = null
+    //파이어스토어
     var firestore: FirebaseFirestore? = null
-    var user: FirebaseUser? = null
+    private val currentUser = com.google.firebase.ktx.Firebase.auth.currentUser?.uid
+
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
@@ -40,16 +49,24 @@ class HomeFragment : Fragment() {
     private val PREF_NAME = "ButtonPress"
     private val PREF_KEY_HAS_ATTENDED = "HasAttended"
 
+
+    private lateinit var adapter: HomeFragmentAdapter
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-
-        auth = FirebaseAuth.getInstance()
-        user = auth!!.currentUser
+        // 파이어스토어 인스턴스 초기화
         firestore = FirebaseFirestore.getInstance()
 
+        adapter = HomeFragmentAdapter()
+        binding.rvTodoMain.adapter = adapter
+        binding.rvTodoMain.layoutManager = LinearLayoutManager(requireContext())
+
+        // 데이터를 불러오는 코드를 onCreateView 내에서 실행
+        loadDataFromFirestore()
         loadTimeDate()
 
         binding.ivGoAlarm.setOnClickListener {
@@ -62,6 +79,58 @@ class HomeFragment : Fragment() {
         }
 
         return binding.root
+    }
+    inner class HomeFragmentAdapter : RecyclerView.Adapter<HomeFragmentAdapter.ViewHolder>() {
+        private var list = ArrayList<MyGoal>()
+
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): HomeFragmentAdapter.ViewHolder {
+            return ViewHolder(
+                ItemTodoMainBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            )
+        }
+
+        override fun onBindViewHolder(holder: HomeFragmentAdapter.ViewHolder, position: Int) {
+            val item = list[position]
+            holder.bind(item)
+        }
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+        inner class ViewHolder(private val binding : ItemTodoMainBinding) : RecyclerView.ViewHolder(binding.root) {
+            fun bind(item: MyGoal) = with(binding) {
+                tvTodo.text = item.title
+                checkBox.isChecked= item.check
+            }
+        }
+
+        fun addToList(item: MyGoal) {
+            this.list.add(item)
+        }
+        fun clearList() {
+            this.list.clear()
+        }
+
+    }
+    private fun loadDataFromFirestore() {
+        firestore?.collection("UserDto")?.document("KWler36V6MdaNkMsvtK2DRynWVw1")
+            ?.collection("MyGoal")
+            ?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                // ArrayList 비워줌
+                adapter.clearList()
+
+                for (snapshot in querySnapshot!!.documents) {
+                    var item = snapshot.toObject(MyGoal::class.java)
+                    if(item != null) {
+                        adapter.addToList(item)
+                    }
+                }
+                adapter.notifyDataSetChanged()
+            }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -106,8 +175,12 @@ class HomeFragment : Fragment() {
             Toast.makeText(requireContext(), "이미 출석했습니다.", Toast.LENGTH_SHORT).show()
             Log.d("확인", binding.btnAttend.isEnabled.toString())
         }
-    }
 
+        // RecyclerView 어댑터 초기화 및 데이터 불러오는 코드 이곳으로 이동
+//        adapter = HomeFragmentAdapter()
+//        binding.rvTodoMain.adapter = adapter
+//        binding.rvTodoMain.layoutManager = LinearLayoutManager(requireContext())
+    }
     private fun loadHasAttended(): Boolean {
         val preferences = requireContext().getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
         return preferences.getBoolean(PREF_KEY_HAS_ATTENDED, false)
