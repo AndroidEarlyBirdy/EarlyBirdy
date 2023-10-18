785776e15f51aceb0de7047704045f6227f891a1
SeungYoonPark <139108875+SeungYoonPark@users.noreply.github.com>
2023-10-18 14:14:11 +0900

    feat : SettingActivity 알림설정 기능 추가

--- app/src/main/java/com/example/earlybirdy/setting/SettingActivity.kt
+++ app/src/main/java/com/example/earlybirdy/setting/SettingActivity.kt
@@ -1,7 +1,12 @@
 package com.example.earlybirdy.setting
 
+import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.earlybirdy.databinding.ActivitySettingBinding
 
@@ -26,7 +31,42 @@ class SettingActivity : AppCompatActivity() {
 
         }
 
+        // 알림 설정 버튼 클릭 시 알림 설정 화면으로 이동
+        binding.btnNotificationSettings.setOnClickListener {
+            presentNotificationSetting(this)
 
+        }
+    }
+
+    fun presentNotificationSetting(context: Context) {
+        val intent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationSettingOreo(context)
+        } else {
+            notificationSettingOreoLess(context)
+        }
+        try {
+            context.startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            e.printStackTrace()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun notificationSettingOreo(context: Context): Intent {
+        return Intent().also { intent ->
+            intent.action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
+            intent.putExtra(Settings.EXTRA_APP_PACKAGE, context.packageName)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+    }
+
+    fun notificationSettingOreoLess(context: Context): Intent {
+        return Intent().also { intent ->
+            intent.action = "android.settings.APP_NOTIFICATION_SETTINGS"
+            intent.putExtra("app_package", context.packageName)
+            intent.putExtra("app_uid", context.applicationInfo?.uid)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
     }
 
     private fun navigateToDetail(title: String, content: String) {
