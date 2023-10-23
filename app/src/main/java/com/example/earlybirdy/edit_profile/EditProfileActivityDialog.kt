6ebfaceaa447823e5b34805770dcd81a10771aac
Odin5din <133902344+Odin5din@users.noreply.github.com>
2023-10-23 09:18:02 +0900

    feature: EditProfileActivity 사용자 인증 이메일 받아오기 완료

--- app/src/main/java/com/example/earlybirdy/edit_profile/EditProfileActivityDialog.kt
+++ app/src/main/java/com/example/earlybirdy/edit_profile/EditProfileActivityDialog.kt
@@ -10,11 +10,16 @@ import com.example.earlybirdy.databinding.ActivityEditProfileDialogBinding
 import com.example.earlybirdy.main.MainActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class EditProfileActivityDialog (
     context: Context,
 ) : Dialog(context) {
     private lateinit var binding: ActivityEditProfileDialogBinding
+    private lateinit var user: FirebaseUser
+    val db = Firebase.firestore
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,42 +37,60 @@ class EditProfileActivityDialog (
     }
 
     private fun initViews() = with(binding) {
-        binding.btnEpdSignin.setOnClickListener{
-            checkAuth()
-        }
-
-    }
-    private fun checkAuth() {
-        val email = binding.titEpdEmail.text.toString()
-        val password = binding.titEpdPassword.text.toString()
-
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    // 인증 성공: 입력한 이메일과 비밀번호가 일치함
-                    val user = FirebaseAuth.getInstance().currentUser
-                    Toast.makeText(context, "비밀번호가 확인되었습니다.", Toast.LENGTH_SHORT).show()
-                    Log.d("login","success")
-                    dismiss()
-                } else {
-                    // 인증 실패: 입력한 이메일과 비밀번호가 일치하지 않음
-                    val exception = task.exception
-                    if (exception is FirebaseAuthInvalidCredentialsException) {
-                        // 잘못된 자격 증명 오류 처리
-                        Toast.makeText(context, "이메일 또는 비밀번호가 올바르지 않습니다.", Toast.LENGTH_SHORT).show()
-                        Log.d("log","잘못된 자격 증명 오류 처리")
-                        context.startActivity(Intent(context, MainActivity::class.java))
-
-
-
-                    } else {
-                        Toast.makeText(context, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
-                        Log.d("log","그 외")
-                        context.startActivity(Intent(context, MainActivity::class.java))
-                    }
-                }
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            loadUserData(user.uid)
+            binding.btnEpdSignin.setOnClickListener {
+                checkAuth(user.email ?: "", binding.titEpdPassword.text.toString())
             }
-
+        } else {
+            dismiss()
+            Toast.makeText(context,"로그인을 해주세요!",Toast.LENGTH_SHORT).show()
+        }
     }
 
-}
+    private fun loadUserData(uid: String) {
+
+        db.collection("UserDto").document(uid).get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    val loadEmail = document.getString("email") ?: ""
+                    binding.titEpdEmail.setText(loadEmail)
+                }
+            }.addOnFailureListener{
+                Log.e("loadUserData", "Failed to load user data", it)
+            }
+    }
+    private fun checkAuth(email:String,password:String) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            // 인증 성공: 입력한 이메일과 비밀번호가 일치함
+                            val user = FirebaseAuth.getInstance().currentUser
+                            Toast.makeText(context, "비밀번호가 확인되었습니다.", Toast.LENGTH_SHORT).show()
+                            Log.d("login", "success")
+                            dismiss()
+                        } else {
+                            // 인증 실패: 입력한 이메일과 비밀번호가 일치하지 않음
+                            val exception = task.exception
+                            if (exception is FirebaseAuthInvalidCredentialsException) {
+                                // 잘못된 자격 증명 오류 처리
+                                Toast.makeText(
+                                    context,
+                                    "이메일 또는 비밀번호가 올바르지 않습니다.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                Log.d("log", "잘못된 자격 증명 오류 처리")
+                                context.startActivity(Intent(context, MainActivity::class.java))
+
+
+                            } else {
+                                Toast.makeText(context, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                                Log.d("log", "그 외")
+                                context.startActivity(Intent(context, MainActivity::class.java))
+                            }
+                        }
+                    }
+
+            }
+    }
