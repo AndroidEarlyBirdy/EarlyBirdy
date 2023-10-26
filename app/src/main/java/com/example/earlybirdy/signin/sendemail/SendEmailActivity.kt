b89e58812ecafb2cb1a3a6a3af9452524743f7d3
LeeChoongHwan <choo901@naver.com>
2023-10-26 17:48:55 +0900

    refactor : SendEmailActivity ViewModel 적용

--- app/src/main/java/com/example/earlybirdy/signin/sendemail/SendEmailActivity.kt
+++ app/src/main/java/com/example/earlybirdy/signin/sendemail/SendEmailActivity.kt
@@ -1,28 +1,23 @@
 package com.example.earlybirdy.signin.sendemail
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.activity.viewModels
 import com.example.earlybirdy.databinding.ActivitySendEmailBinding
-import com.example.earlybirdy.databinding.ActivitySigninBinding
 import com.example.earlybirdy.main.MainActivity
-import com.example.earlybirdy.util.navigateToMainActivity
 import com.example.earlybirdy.util.navigateToSigninActivity
 import com.example.earlybirdy.util.navigateToSignupActivity
 import com.example.earlybirdy.util.showToast
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
-import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class SendEmailActivity : MainActivity() {
     private val binding by lazy { ActivitySendEmailBinding.inflate(layoutInflater) }
+    private val sendEmailViewModel: SendEmailViewModel by viewModels()
 
     private lateinit var auth: FirebaseAuth
     private lateinit var database: DatabaseReference
-    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -43,7 +38,6 @@ class SendEmailActivity : MainActivity() {
 
         // 회원가입 페이지로 이동
         binding.tvBtnSignup.setOnClickListener {
-            //val intent = Intent(Intent.ACTION_VIEW, Uri.parse("${}"))
             navigateToSignupActivity(this)
         }
     }
@@ -53,20 +47,12 @@ class SendEmailActivity : MainActivity() {
         val user = auth.currentUser
         user?.reload() // 최신 유저 정보 갱신
         if (user != null) {
-            //navigateToMainActivity(this)
-            showToast(this, "이미 로그인 중")
+            showToast(this, "이미 로그인 중 입니다.")
         }
         else {
             val emailAddress = binding.titSendEmail.text.toString()
             if (emailAddress.isNotBlank()) {
-                Firebase.auth.sendPasswordResetEmail(emailAddress)
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            Log.d("send", "${emailAddress}")
-                        }
-                    }.addOnFailureListener {
-                        // 유저 정보에 없는 이메일이면 보내지 않음
-                    }
+                sendEmailViewModel.sendEmail(emailAddress)
             }
         }
     }
