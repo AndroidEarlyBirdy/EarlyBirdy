ad1cb8f270203d02cb86ecfb00703441cc60f67c
LeeChoongHwan <choo901@naver.com>
2023-10-26 16:39:09 +0900

    refactor : Signin 함수명 수정 / SendEmailActivity ViewModel 적용

--- app/src/main/java/com/example/earlybirdy/signin/SignInViewModel.kt
+++ app/src/main/java/com/example/earlybirdy/signin/SignInViewModel.kt
@@ -5,27 +5,27 @@ import androidx.lifecycle.ViewModel
 import com.example.earlybirdy.sealedclass.SigninNavigation
 import com.google.firebase.auth.FirebaseAuth
 
-class SigninViewModel: ViewModel() {
+class SignInViewModel: ViewModel() {
     private val auth = FirebaseAuth.getInstance()
 
-    val signinState = MutableLiveData<SigninNavigation>()
+    val signInState = MutableLiveData<SigninNavigation>()
 
-    fun signin(email: String, password: String) {
+    fun signIn(email: String, password: String) {
         val user = auth.currentUser
         if (user != null) { // 로그인 여부 체크
-            signinState.value = SigninNavigation.AlreadySignin
+            signInState.value = SigninNavigation.AlreadySignin
         } else {
             if (email.isEmpty()){ // 이메일 입력 여부
-                signinState.value = SigninNavigation.EmptyEmail("이메일을 입력해주세요")
+                signInState.value = SigninNavigation.EmptyEmail("이메일을 입력해주세요")
             }else if (password.isEmpty()){
-                signinState.value = SigninNavigation.EmptyPassword("비밀번호를 입력해주세요")
+                signInState.value = SigninNavigation.EmptyPassword("비밀번호를 입력해주세요")
             }else{
                 auth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener() { task ->
                         if (task.isSuccessful) {
-                            signinState.value = SigninNavigation.SigninSuccess
+                            signInState.value = SigninNavigation.SigninSuccess
                         } else {
-                            signinState.value = SigninNavigation.SigninFailed("로그인에 실패하였습니다. 이메일과 비밀번호를 확인해주세요.")
+                            signInState.value = SigninNavigation.SigninFailed("로그인에 실패하였습니다. 이메일과 비밀번호를 확인해주세요.")
                         }
                     }
             }
