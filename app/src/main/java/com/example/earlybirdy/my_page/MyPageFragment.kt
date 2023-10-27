41a449c2361459b75af0809bc99610fc0cbe8308
minji05 <smj95133@likelion.org>
2023-10-27 15:51:54 +0900

    feature : 커스텀 달력 출석 구현

--- app/src/main/java/com/example/earlybirdy/my_page/MyPageFragment.kt
+++ app/src/main/java/com/example/earlybirdy/my_page/MyPageFragment.kt
@@ -1,171 +1,108 @@
 package com.example.earlybirdy.my_page
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.Fragment
 import com.example.earlybirdy.R
-import com.example.earlybirdy.databinding.FragmentMyPageBinding
-import com.example.earlybirdy.util.navigateToEditProfileActivity
+import com.example.earlybirdy.home.HomeFragment
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.DayViewDecorator
 import com.prolificinteractive.materialcalendarview.DayViewFacade
-import com.example.earlybirdy.util.navigateToSettingActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Locale
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import com.prolificinteractive.materialcalendarview.spans.DotSpan
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 
 class MyPageFragment : Fragment() {
 
-    private var _binding: FragmentMyPageBinding? = null
-    private val binding get() = _binding!!
-
-    companion object {
-        fun newInstance() = MyPageFragment()
-    }
-
-    //Firebase
-    private lateinit var auth: FirebaseAuth
-    private lateinit var user: FirebaseUser
-
-    //ViewModel
-    private lateinit var myPageViewModel : MyPageViewModel
+    private lateinit var calendarView: MaterialCalendarView
+    private val firestore = FirebaseFirestore.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentMyPageBinding.inflate(inflater, container, false)
-        return binding.root
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_my_page, container, false)
+        calendarView = view.findViewById(R.id.calendarView)
+
+
+        loadAttendanceData()
+
+        return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun loadAttendanceData() {
+        val userId = "alUKQs4TIDM7F6GFC01TfXdUWwB2"
 
-        auth = FirebaseAuth.getInstance()
-        user = auth.currentUser!!
-        myPageViewModel = ViewModelProvider(this, MyPageViewModelFactory())[MyPageViewModel::class.java]
 
-        getData()
-        observeData()
-        setListener()
-    }
+        firestore.collection("UserDto")
+            .document(userId)
+            .collection("Attendance")
+            .get()
+            .addOnSuccessListener { querySnapshot: QuerySnapshot? ->
+                for (document in querySnapshot!!) {
+                    val date = document.getString("date")
+                    if (date != null) {
+                        val calendarDay = parseDate(date)
+                        val dayDecorator = DayDecorator(calendarDay, requireContext())
+                        calendarView.addDecorator(dayDecorator)
+                    }
+                }
 
-    //Repository에서 데이터 get
-    private fun getData() {
-        myPageViewModel.getMyPageUserData(user.uid)
-        myPageViewModel.getAttendanceData(user.uid)
-    }
-
-    //LiveData 감지
-    @SuppressLint("SetTextI18n")
-    private fun observeData() {
-        myPageViewModel.userData.observe(viewLifecycleOwner) { user->
-            binding.tvNickname.text = user.nickname
-            user.exp?.let { myPageViewModel.initializeUIWithLocalExp(it) }
-            user.profile?.let { setProfileImage(it) }
-        }
-
-        myPageViewModel.attendanceData.observe(viewLifecycleOwner) {list ->
-            val dateList = ArrayList<CalendarDay>()
-            for(date in list) {
-                convertStringToCalendarDay(date.date)?.let { dateList.add(it) }
             }
-            Log.d("dateList",dateList.toString())
-            binding.calendarView.addDecorator(Decorator(dateList, requireContext()))
-        }
-
-        myPageViewModel.expMap.observe(viewLifecycleOwner) { map ->
-            val currentExp = map["exp"]
-            val currentLevel = map["level"]
-            val currentMapExp = map["maxExp"]
-
-            binding.pbExp.progress = currentExp!!
-            binding.tvLevel.text = "레벨 $currentLevel"
-            binding.pbExp.max = currentMapExp!!
-            binding.tvExperience.text = "$currentExp / $currentMapExp xp"
-
-            updateLevelImage(currentLevel!!)
-        }
     }
 
-    //리스너 부착
-    private fun setListener() {
-        binding.ivSetting.setOnClickListener {
-            navigateToSettingActivity(requireContext())
-        }
-        binding.ivEditProfile.setOnClickListener {
-            navigateToEditProfileActivity(requireContext())
-        }
+    // 날짜를 CalendarDay로 변환
+    private fun parseDate(date: String): CalendarDay {
+        val year = date.substring(0, 4).toInt()
+        val month = date.substring(5, 7).toInt()
+        val day = date.substring(8, 10).toInt()
+        return CalendarDay.from(year, month, day)
     }
 
-    //레벨 범위에 따른 인장 설정
-    private fun updateLevelImage(level : Int) {
-        when (level) {
-            in 1..10 -> binding.ivProfileBorder1.setImageResource(R.drawable.ic_insignia1)
-            in 11..20 -> binding.ivProfileBorder1.setImageResource(R.drawable.ic_insignia2)
-            in 21..30 -> binding.ivProfileBorder1.setImageResource(R.drawable.ic_insignia3)
-            in 31..40 -> binding.ivProfileBorder1.setImageResource(R.drawable.ic_insignia4)
-            else -> binding.ivProfileBorder1.setImageResource(R.drawable.ic_insignia5)
-        }
-    }
+//    // 날짜에 따라 배경 이미지 추가
+//    private fun checkGoalsForDate(date: CalendarDay) {
+//        val userId = "사용자 UID" // 사용자의 UID로 변경
+//
+//        // Firestore에서 사용자의 목표 데이터 가져오기
+//        firestore.collection("UserDto")
+//            .document(userId)
+//            .collection("MyGoal")
+//            .get()
+//            .addOnSuccessListener { querySnapshot ->
+//                for (document in querySnapshot) {
+//                    val goalDate = document.getString("date")
+//                    val checked = document.getBoolean("check") ?: false
+//
+//                    if (goalDate != null && parseDate(goalDate) == date && checked) {
+//                        // 해당 날짜에 목표가 달성되고 체크된 경우, 배경 이미지를 추가
+//                        calendarView.addDecorator(DayDecorator(date, R.drawable.bg_calendar_date))
+//                        break // 이미 배경 이미지를 추가했으므로 루프 종료
+//                    }
+//                }
+//            }
+//    }
 
-    private fun setProfileImage(profileNum : Int) {
-        when(profileNum) {
-            1 -> binding.ivProfile.setImageResource(R.drawable.img_profile_man1)
-            2 -> binding.ivProfile.setImageResource(R.drawable.img_profile_woman1)
-            3 -> binding.ivProfile.setImageResource(R.drawable.img_profile_man2)
-            4 -> binding.ivProfile.setImageResource(R.drawable.img_profile_woman2)
-            else -> binding.ivProfile.setImageResource(R.drawable.img_profile_add)
-        }
-    }
+    // 날짜에 따라 배경 이미지 추가
+    private class DayDecorator(private val date: CalendarDay, private val context: Context) : DayViewDecorator {
 
-    //String 값을 CalendarDay로 전환
-    private fun convertStringToCalendarDay(dateString: String): CalendarDay? {
-        val dateFormat = SimpleDateFormat("yyyy.MM.dd", Locale.getDefault())
-        return try {
-            val date = dateFormat.parse(dateString)
-            val calendar = Calendar.getInstance()
-            if (date != null) {
-                calendar.time = date
-            }
-            val year = calendar.get(Calendar.YEAR)
-            val month = calendar.get(Calendar.MONTH) + 1
-            val day = calendar.get(Calendar.DAY_OF_MONTH)
+        private val drawable = context.resources.getDrawable(R.drawable.bg_calendar_date1)
 
-            CalendarDay.from(year, month, day)
-        } catch (e: Exception) {
-            null
-        }
-    }
-
-    override fun onDestroyView() {
-        _binding = null
-        super.onDestroyView()
-    }
-
-    //날짜 Custom Decorator
-    class Decorator(dates: List<CalendarDay>, context: Context) : DayViewDecorator {
-
-        private val selectedDates = dates
-
-        @SuppressLint("UseCompatLoadingForDrawables")
-        private val drawable = context.getDrawable(R.drawable.bg_calendar_date)
         override fun shouldDecorate(day: CalendarDay?): Boolean {
-            return selectedDates.contains(day)
+            return day == date
         }
 
-        override fun decorate(view: DayViewFacade?) {
-            if (drawable != null) {
-                view?.setBackgroundDrawable(drawable)
-            }
+        override fun decorate(view: DayViewFacade) {
+            view.setBackgroundDrawable(drawable)
         }
+    }
+
+
+
+    companion object {
+        fun newInstance() = MyPageFragment()
     }
 }
