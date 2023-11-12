d3576938c1e44c9e6642850a0c5df3921fb99d28
LeeChoongHwan <choo901@naver.com>
2023-11-12 12:02:46 +0900

    refactor : Board

--- app/src/main/java/com/nbcproject/earlybirdy/board/board_write/BoardWriteActivity.kt
+++ app/src/main/java/com/nbcproject/earlybirdy/board/board_write/BoardWriteActivity.kt
@@ -5,14 +5,11 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
 import com.bumptech.glide.Glide
-import com.nbcproject.earlybirdy.R
 import com.nbcproject.earlybirdy.board.board_read.BoardReadActivity
 import com.nbcproject.earlybirdy.databinding.ActivityBoardWriteBinding
 import com.nbcproject.earlybirdy.dto.BoardDto
@@ -34,18 +31,17 @@ class BoardWriteActivity : MainActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var database: DatabaseReference
-    val db = Firebase.firestore
+    private val db = Firebase.firestore
     private val fireStore = FirebaseFirestore.getInstance()
     private val storage = FirebaseStorage.getInstance()
 
     private var nickname: String? = ""
 
     private var imgUri: Uri = "".toUri()
-    private val IMAGE_PICKER_REQUEST_CODE = 1
 
-    val storageRef = storage.reference
+    private val storageRef = storage.reference
 
-    var photoCheck: Boolean = false
+    private var photoCheck: Boolean = false
 
     private val boardType: Int by lazy {
         intent.getIntExtra("boardType", 1)
@@ -99,14 +95,6 @@ class BoardWriteActivity : MainActivity() {
                 ) == PackageManager.PERMISSION_GRANTED -> {
                     navigateGallery()
                 }
-//                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
-//                    showPermissionContextPopup()
-//                }
-//                //갤러리 접근 권한 설정
-//                else -> requestPermissions(
-//                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
-//                    1000
-//                )
             }
         }
     }
@@ -147,35 +135,22 @@ class BoardWriteActivity : MainActivity() {
         pickImageActivityResult.launch(intent)
     }
 
-    //권한 설정 팝업
-    private fun showPermissionContextPopup() {
-        AlertDialog.Builder(this)
-            .setTitle(getString(R.string.modify_permissionRequired))
-            .setMessage(getString(R.string.modify_profilePermission))
-            .setPositiveButton(getString(R.string.modify_agree)) { _, _ ->
-                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
-            }
-            .setNegativeButton(getString(R.string.modify_cancel)) { _, _ -> }
-            .create()
-            .show()
-    }
-
     private fun createdBoard() {
         val user = auth.currentUser
 
-        var boardIndex = UUID.randomUUID().toString()
+        val boardIndex = UUID.randomUUID().toString()
         val createdTime = Timestamp(Date())
 
         val contentsTitle = binding.etContentsTitle.text.toString()
         val contents = binding.etContents.text.toString()
 
-        var contentsPhoto: String? = null
+        var contentsPhoto: String?
 
-        var storage: FirebaseStorage? = FirebaseStorage.getInstance()
-        var fileName = boardIndex
+        val storage: FirebaseStorage? = FirebaseStorage.getInstance()
+        val fileName = boardIndex
 
         if (photoCheck){
-            var imagesRef = storage!!.reference.child(boardIndex).child(fileName)
+            val imagesRef = storage!!.reference.child(boardIndex).child(fileName)
 
             imagesRef.putFile(imgUri).addOnSuccessListener { taskSnapshot ->
                 taskSnapshot.metadata?.reference?.downloadUrl?.addOnSuccessListener { it ->
@@ -222,8 +197,7 @@ class BoardWriteActivity : MainActivity() {
                     )
                 db.collection("BoardDto").document(boardIndex)
                     .set(boardDto)
-                    .addOnSuccessListener { documentReference ->
-                        Log.d("boardDto", "${boardDto}")
+                    .addOnSuccessListener {
                         finish()
                     }
             }
@@ -254,8 +228,8 @@ class BoardWriteActivity : MainActivity() {
 
         var contentsPhoto: String? = boardData.contentsPhoto
 
-        if (photoCheck){
-            var imagesRef = storage!!.reference.child(boardData.bid).child(boardData.bid)
+        if (photoCheck) {
+            val imagesRef = storage.reference.child(boardData.bid).child(boardData.bid)
 
             imagesRef.putFile(imgUri).addOnSuccessListener { taskSnapshot ->
                 taskSnapshot.metadata?.reference?.downloadUrl?.addOnSuccessListener { it ->
@@ -267,47 +241,36 @@ class BoardWriteActivity : MainActivity() {
                     } else if (contents.isEmpty()) {
                         binding.etContents.error = "내용을 입력해주세요"
                     } else {
-                        val boardDto =
-                            BoardDto(
-                                boardData.bid,
-                                boardData.uid,
-                                nickname!!,
-                                boardData.createdTime,
-                                contentsTitle,
-                                contents,
-                                contentsPhoto
-                            )
-                        db.collection("BoardDto").document(boardData.bid)
-                            .set(boardDto)
-                            .addOnSuccessListener { documentReference ->
-                                finish()
-                            }
+                        updateBoardDatabase(boardData, contentsTitle, contents, contentsPhoto)
                     }
                 }
             }
-        }else{
+        } else {
             if (contentsTitle.isEmpty()) {
                 binding.etContentsTitle.error = "제목을 입력해주세요"
             } else if (contents.isEmpty()) {
                 binding.etContents.error = "내용을 입력해주세요"
             } else {
-                val boardDto =
-                    BoardDto(
-                        boardData.bid,
-                        boardData.uid,
-                        nickname!!,
-                        boardData.createdTime,
-                        contentsTitle,
-                        contents,
-                        contentsPhoto
-                    )
-                db.collection("BoardDto").document(boardData.bid)
-                    .set(boardDto)
-                    .addOnSuccessListener { documentReference ->
-                        Log.d("boardDto", "${boardDto}")
-                        finish()
-                    }
+                updateBoardDatabase(boardData, contentsTitle, contents, contentsPhoto)
             }
         }
+    }
+
+    private fun updateBoardDatabase(boardData : BoardDto, contentsTitle : String,contents : String, contentsPhoto : String?) {
+        val boardDto =
+            BoardDto(
+                boardData.bid,
+                boardData.uid,
+                nickname!!,
+                boardData.createdTime,
+                contentsTitle,
+                contents,
+                contentsPhoto
+            )
+        db.collection("BoardDto").document(boardData.bid)
+            .set(boardDto)
+            .addOnSuccessListener {
+                finish()
+            }
     }
 }
